a4543285575a6cf99da28330585bc2390bea8588
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-09-01 22:37:11 +0300

    HW DONE +/-

---
+++ Blazor/Blazor/Components/Pages/Fibona44i.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Fibona44i
+	{
+		private List<string> List_of_Fibona44i { get; set; } = new List<string>() { };
+		int end_fibona44i = 0;
+		void Fibona44i_f()
+		{
+			List_of_Fibona44i.Clear();
+			BigInteger temp_value;
+			BigInteger fibona44i_1 = 0;
+			BigInteger fibona44i_2 = 1;
+			List_of_Fibona44i.Add(fibona44i_1.ToString());
+
+			while (end_fibona44i >= fibona44i_2)
+			{
+				List_of_Fibona44i.Add(fibona44i_2.ToString());
+				temp_value = fibona44i_2;
+				fibona44i_2 += fibona44i_1;
+				fibona44i_1 = temp_value;
+			}
+		}
+	}
+}
---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor.Components.Pages
+{
+	public partial class Power
+	{
+		public int num_in = 1;
+		int num_power = 1;
+		double rezult = 0;
+		void Power_calc()
+		{
+			rezult = Math.Pow(num_in, num_power);
+		}
+	}
+}
