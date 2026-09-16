9c0352832814e6b36b8c03ec215c55fb9308dfaa
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-09-16 20:27:22 +0300

    lesson 44 Scafold Student

--- Blazor/Academy/Models/Student.cs
+++ Blazor/Academy/Models/Student.cs
@@ -3,32 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Academy.Models
 {
-	public class Student
+	public class Student:Human
 	{
 		[Key]
-		[Required]
 		[Column("stud_id")]
-
 		public int stud_id { get; set; }
 
 		[Required]
-		public string last_name { get; set; }
-
-		[Required]
-		public string first_name { get; set; }
-
-		public string? middle_name { get; set; }
-
-		[Required]
-		[Column("birth_date")]
-		public DateOnly? BirthDate { get; set; }
-
-		public string? email { get; set; }
-		public string? phone { get; set; }
-
-		[Column("photo", TypeName = "IMAGE")]
-		public byte[]? Photo { get; set; }
-
 		[ForeignKey("Group")]
 		public int? group { get; set; }
 	}
