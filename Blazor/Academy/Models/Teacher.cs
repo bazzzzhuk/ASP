66e094857fcdcb3a11f6c5f8e22764419896ee4b
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2026-09-25 20:52:06 +0300

    lesson 2 - TODO Disciplines for teachers

--- Blazor/Academy/Models/Teacher.cs
+++ Blazor/Academy/Models/Teacher.cs
@@ -3,32 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Academy.Models
 {
-	public class Teacher
+	public class Teacher:Human
 	{
-		[Required]
 		[Key]
-		[Column(TypeName = "SMALLINT")]
-		public int Teacher_id { get; set; }
-		[Required]
-		public string? Last_name { get; set; }
+		[Column("teacher_id", TypeName = "SMALLINT")]
+		public int teacher_id { get; set; }
 
-		[Required]
-		public string? First_name { get; set; }
+		public DateOnly? work_since { get; set; }
 
-		public string? Middle_name { get; set; }
-
-		[Required]
-		[Column("birth_date")]
-		public DateOnly? BirthDate { get; set; }
-
-		public string? Email { get; set; }
-		public string? Phone { get; set; }
-
-		[Column("photo", TypeName = "IMAGE")]
-		public byte[]? Photo { get; set; }
-		public DateOnly? Work_since { get; set; }
-
+		[DataType(DataType.Currency)]
 		[Column(TypeName = "SMALLMONEY")]
 		public decimal? Rate { get; set; }
+
+		//Navigation properties:
 	}
 }
---
+++ Blazor/Academy/Models/TeachersDisciplinesRelation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academy.Models
+{
+	[PrimaryKey("teacher","discipline")]
+	public class TeachersDisciplinesRelation
+	{
+		[Column("teacher", TypeName = "SMALLINT")]
+		[ForeignKey(nameof(Teacher))]
+		public int teacher { get; set; }
+		[Column("discipline", TypeName ="SMALLINT")]
+		[ForeignKey(nameof(Discipline))]
+		public int discipline { get; set; }
+
+		//navigation properties:
+		public Teacher Teacher { get; set; }
+		public Discipline Discipline { get; set; }
+	}
+}
